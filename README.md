e1c8a4355cb96a08840e090916e4b3db6fda7338
xuantruongcox <truongcool999@gmail.com>
2024-01-18 20:47:17 +0700

    Get started

---
+++ README.md
@@ -0,0 +1,31 @@
+# @coxdev/cox-quiz
+
+## Installation
+
+```bash
+npm i lit @coxdev/cox-quiz
+```
+
+## Usage
+
+**Vanilla Js**
+
+index.html
+
+```html
+<body>
+  ...
+  <!-- If you want to add a quiz. -->
+  <cox-quiz question='{"name": "How old are you ??", "answers": ["15", "18", "24", "25"]}' correctAnswer="24"></cox-quiz>
+  <script type="module">
+    import "@coxdev/cox-quiz";
+  </script>
+</body>
+```
+
+## Options
+The configuration component can contain the following reactive properties:
+- `question` (required): An object or a JSON string
+    - `name`: An string, The name for question
+    - `answers`: An array of string
+- `correctAnswer`: An String
---
+++ src/cox-quiz.ts
@@ -0,0 +1,148 @@
+import { LitElement, css, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { when } from "lit/directives/when.js";
+
+class QuizForm {
+  name: String;
+  answers: Array<String>;
+  constructor(
+    _name: String,
+    _answers: Array<String>,
+  ) {
+    this.name = _name;
+    this.answers = _answers;
+  }
+}
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+
+@customElement("cox-quiz")
+export class CoxQuiz extends LitElement {
+  static styles = css`
+    h3 {
+      text-align: center;
+    }
+    ul {
+      list-style-type: none;
+    }
+    .quiz-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .answer__list {
+      display: flex;
+      flex-wrap: wrap;
+      max-width: 400px;
+    }
+    .answer__list-item {
+      flex: 1 0 100px;
+    }
+  `;
+  @property({ type: QuizForm })
+  question = {
+    name: "What color is the dog ???",
+    answers: ["blue", "black", "red", "yellow"],
+  };
+  @property({type: String})
+  correctAnswer = 'black';
+
+  private isPassed = false;
+  private isSelected = false;
+  
+  render() {
+    if(typeof this.question === "string"){
+      const convert = JSON.parse(this.question);
+      this.question = convert
+    }
+    return html`
+      ${when(
+        this.isPassed,
+        () => html`
+          <div
+            style="display: flex; flex-direction: column; align-items:center"
+          >
+            <h1 style="text-align: center">Passed</h1>
+            <button style="max-width: 50px" @click="${this.resetQuiz}">
+              Reset
+            </button>
+          </div>
+        `,
+        () => {
+          return html`
+            <div class="quiz-container">
+              <h3>${this.question.name}</h3>
+              <ul class="answer__list">
+                ${this.question.answers.map((answer, answerIndex) => {
+                  return html`
+                    <li class="answer__list-item">
+                      <div class="item__checkbox">
+                        <input
+                          ?disabled=${this.isSelected}
+                          @input=${this.onSelected}
+                          id="${answer}"
+                          name="${this.question}"
+                          type="radio"
+                          value="${answer}"
+                        />
+                        <label for="${answer}"
+                          >${this.numberToAlphabet(
+                            answerIndex + 1
+                          )}.${answer}</label
+                        >
+                      </div>
+                    </li>
+                  `;
+                })}
+              </ul>
+              <h1>${this.message}</h1>
+              <button ?hidden=${!this.isSelected} style="max-width: 50px" @click="${this.resetQuiz}">
+                Reset
+              </button>
+            </div>
+          `;
+        }
+      )}
+    `;
+  }
+
+  private numberToAlphabet(number: number) {
+    if (Number.isInteger(number) && number > 0) {
+      return String.fromCharCode(number + 64);
+    } else {
+      return "Invalid input. Please provide a positive integer.";
+    }
+  }
+
+  private message = "";
+  private onSelected(e: Event) {
+    e.preventDefault();
+    const input = e.target as HTMLInputElement;
+    const isCorrect = this.correctAnswer === input.value;
+    if (isCorrect) {
+      this.isPassed = !this.isPassed;
+      this.isSelected = !this.isSelected;
+      this.requestUpdate();
+    } else {
+      this.message = "Your answer is incorrect";
+      this.isSelected = !this.isSelected;
+      this.requestUpdate();
+    }
+  }
+  private resetQuiz() {
+    this.isPassed = false;
+    this.isSelected = false;
+    this.message = '';
+    this.requestUpdate();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "cox-quiz": CoxQuiz;
+  }
+}
